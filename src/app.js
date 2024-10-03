97a558f7b4ffd3323d3502e1b656278bacd96ef8
Touhid Zaman <79441219+touhidzaman007@users.noreply.github.com>
2024-10-03 15:03:43 +0600

    all setup configured

--- src/app.js
+++ src/app.js
@@ -21,4 +21,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// Routes Import
+import userRouter from "./routes/user.routes.js";
+
+// Routes Declaration
+app.use("/api/v1/users", userRouter);
+
 export { app };
